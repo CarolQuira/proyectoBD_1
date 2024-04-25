6642824ea193c26928e4ce216f5121bbcd939462
CarolQuira <carol.quira@uao.edu.co>
2024-04-25 16:23:09 -0500

    Agrega Ciudades

---
+++ proyectoBD_1/models/Ciudades.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace proyectoBD_1.models
+{
+    class Ciudades()
+	{
+        private int idCiudad;
+        private string nombreCiudad;
+        private int idPaisC;
+
+        public Ciudades(int idCiudad, string nombreCiudad, int idPaisC)
+        {
+            this.idCiudad = idCiudad;
+            this.nombreCiudad = nombreCiudad;   
+            this.idPaisC = idPaisC;
+        }
+
+        public Ciudades()
+        {
+
+        }
+
+    }
+}
