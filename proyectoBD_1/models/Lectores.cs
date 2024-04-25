735ed160ac08651c84afa86acf32ad62dffc5b0e
joansalcedo1 <sebastiansalcedo7@gmail.com>
2024-04-25 10:17:40 -0500

    Comentario
    un comentario

--- proyectoBD_1/models/Lectores.cs
+++ proyectoBD_1/models/Lectores.cs
@@ -8,7 +8,7 @@ namespace proyectoBD_1.models
 {
     class Lectores
     {
-
+        //maldito github
         private int idLector;
         private string nombreL1;
         private string nombreL2;
