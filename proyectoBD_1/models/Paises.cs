2da5149b4cc3b3452a341ab24ac1cc81a48500e1
CarolQuira <carol.quira@uao.edu.co>
2024-04-25 16:08:56 -0500

    Agregar paises

---
+++ proyectoBD_1/models/Paises.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyectoBD_1.models
+{
+	class Paises()
+	{
+        private int idPais;
+        private string nombrePais;
+
+        public int IdPais { get => idPais; set => idPais = value;}
+        public string NombrePais { get => nombrePais; set => nombrePais = value; }
+
+        public Paises (int idPais, string nombrePais)
+        {
+            this.idPais = idPais;
+            this.nombrePais = nombrePais;
+        }
+        public Paises()
+        {
+
+        }
+    }
+}
