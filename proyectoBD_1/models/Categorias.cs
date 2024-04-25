e380159eaddc71014432d75452ab82bb2d25f186
joansalcedo1 <sebastiansalcedo7@gmail.com>
2024-04-25 15:01:14 -0500

    Creacion categoria
    Se creo la clase categoria

---
+++ proyectoBD_1/models/Categorias.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace proyectoBD_1.models
+{
+    class Categorias
+    {
+        public int idCategoria;
+        private string nombre;
+        private string fechaCreacion;
+
+        public int idCategoria { get => idCategoria; set => idCategoria = value; }
+        public string nombre { get => nombre; set => nombre = value; }
+        public string fechaCreacion { get => fechaCreacion; set => fechaCreacion = value; }
+    }
+}
