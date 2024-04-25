ef3f492f0f202752167a551e8ecec449bd3f4202
CarolQuira <carol.quira@uao.edu.co>
2024-04-25 16:27:46 -0500

    helpp

--- proyectoBD_1/models/Ciudades.cs
+++ proyectoBD_1/models/Ciudades.cs
@@ -4,9 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
 namespace proyectoBD_1.models
 {
     class Ciudades()
